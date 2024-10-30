edb341f26f060481264395d0f9029030b752c209
GudduBhai81 <306botala@gmail.com>
2024-10-30 23:07:48 +0500

    Add files via upload

---
+++ HomeworkCalenderModule.py
@@ -0,0 +1,6 @@
+import calendar
+
+# Loop through the month numbers (1-12) and print each month name
+for month_num in range(1, 13):
+    month_name = calendar.month_name[month_num]
+    print(month_name)
